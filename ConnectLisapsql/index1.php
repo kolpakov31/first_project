c76e1f6f5bb86e5c05f3f69efb17627fb924f18f
alex <kolpakov71@mail.ru>
2018-12-10 17:36:11 +0600

    qq

---
+++ ConnectLisapsql/index1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Мой сайт DVS</title>
+    <link rel="stylesheet" type="text/css" href="styl.css">
+    <link rel="stylesheet" type="text/css" href="styl1.css">
+    <link rel="stylesheet" type="text/css" href="styl2.css">
+    <script src="jquery.min.js"></script>
+    <script src="script.js"></script>
+</head>
+<body>
+<header class="zag1">
+    Зарегистрируйтесь!!!
+</header>
+
+</body>
+</html>
